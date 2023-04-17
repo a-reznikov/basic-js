a12827ca52f833f3fb3f65c9574274644465190a
a-reznikov <a-reznnikov@gmail.com>
2023-04-18 00:06:30 +0300

    feat: implement solution for mine fix

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,52 +23,52 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(arr) {
-  let result = [];
-  console.log(matrix[1][2]);
- for (let i = 0; i < matrix.length; i++) {
-  let subResult = [];
-  for (let j = 0; j < matrix[i].length; j++)  {
-    let count = 0;
-    if (i === 0 && j === 0) {
+function minesweeper(matrix) {
+//   let result = [];
+//   console.log(matrix[1][2]);
+//  for (let i = 0; i < matrix.length; i++) {
+//   let subResult = [];
+//   for (let j = 0; j < matrix[i].length; j++)  {
+//     let count = 0;
+//     if (i === 0 && j === 0) {
       
-    }
-    if (i > 1 && j > 1) {
-      if (matrix[i - 1][j - 1]) {
-        console.log(matrix[i][j]);
-        count ++;
-      }
-      if (matrix[i - 1][j]) {
-        count ++;
-      }
-      if (matrix[i - 1][j + 1]) {
-        count ++;
-      }
-      if (matrix[i][j - 1]) {
-        count ++;
-      }
-      if (matrix[i][j + 1]) {
-        count ++;
-      }
-      if (matrix[i + 1][j - 1]) {
-        count ++;
-      }
-      if (matrix[i + 1][j]) {
-        count ++;
-      }
-      if (matrix[i + 1][j + 1]) {
-        count ++;
-      }
-    } else if (i < 1 && j >)
+//     }
+//     if (i > 1 && j > 1) {
+//       if (matrix[i - 1][j - 1]) {
+//         console.log(matrix[i][j]);
+//         count ++;
+//       }
+//       if (matrix[i - 1][j]) {
+//         count ++;
+//       }
+//       if (matrix[i - 1][j + 1]) {
+//         count ++;
+//       }
+//       if (matrix[i][j - 1]) {
+//         count ++;
+//       }
+//       if (matrix[i][j + 1]) {
+//         count ++;
+//       }
+//       if (matrix[i + 1][j - 1]) {
+//         count ++;
+//       }
+//       if (matrix[i + 1][j]) {
+//         count ++;
+//       }
+//       if (matrix[i + 1][j + 1]) {
+//         count ++;
+//       }
+//     } else if (i < 1 && j >)
     
-    subResult.push(count);
-  }
-  result.push(subResult);
- }
+//     subResult.push(count);
+//   }
+//   result.push(subResult);
+//  }
 
 
-  console.log(result);
-  return result;
+//   console.log(result);
+//   return result;
 }
 
 
