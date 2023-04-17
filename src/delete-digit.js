53a362bb69c12507684f04373f2fbb73cb824219
a-reznikov <a-reznnikov@gmail.com>
2023-04-17 20:38:02 +0300

    feat: implement solution for delet digit

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,10 +11,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  let arr = String(n).split('');
+  //console.log(arr);
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    let newNumber = [];
+    if (i === 0) {
+      newNumber = arr.slice(1, arr.length);
+    } else if (i === arr.length - 1 && arr.length === 2) {
+      newNumber = arr.slice(0, arr.length - 1);
+    } else {
+      let firstSlice = arr.slice(0, i - 1);
+      let secondSlice = arr.slice(i , arr.length);
+      newNumber = firstSlice.concat(secondSlice);
+    }
+    let numbers = newNumber.join('');
+    newArr.push(+numbers);
+  }
+  //console.log(newArr);
+  let arrRes = newArr.sort((a, b) => a - b);
+  // console.log(arrRes);
+  //console.log('result', arrRes[arrRes.length - 1]);
+  return arrRes[arrRes.length - 1];
 }
+
+deleteDigit(152); // 52);
+deleteDigit(1001); //101);
+deleteDigit(10); //1);
+deleteDigit(222219); //22229);
+deleteDigit(109); //19);
+deleteDigit(342); //42);
+
+// deleteDigit(222219);  //22229)
 
 module.exports = {
   deleteDigit
